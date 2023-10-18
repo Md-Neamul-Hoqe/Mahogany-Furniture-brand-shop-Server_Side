07eeae9ac4702e5ebdf0da3b59bcb81ee07e0a1a
Md Neamul Hoqe <hoqe1997@gmail.com>
2023-10-18 23:13:06 +0600

    Server connected

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+/* middleware */
+app.use(cors());
+app.use(express.json());
+
+// const uri = "https://mahogany-furniture-server-5z8ga4sbo-muhammad-neamul-hoqes-projects.vercel.app";
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.481il7d.mongodb.net/?retryWrites=true&w=majority`;
+const uri = 'mongodb://127.0.0.1:27017';
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+        const productCollection = client.db('Mahogany').collection("furniture");
+        const userCollection = client.db('Mahogany').collection('users');
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+
+    } catch (error) {
+        console.error(error);
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+/* check the app is running  */
+app.get('/', (req, res) => {
+    res.send('App is running');
+})
+
+/* check the server is running */
+app.listen(port, () => {
+    console.log(`App's server is running on PORT: ${port}`);
+})
+
